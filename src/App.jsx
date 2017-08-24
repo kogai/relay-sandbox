e7b6814a1ac07a2ea056ce300cec4993ee9f5fe6
shinichi kogai <kogai0121@gmail.com>
2017-08-24 13:16:52 +0900

    initial

---
+++ src/App.jsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import {
+  QueryRenderer,
+  graphql,
+} from 'react-relay';
+import {
+  Environment,
+  Network,
+  RecordSource,
+  Store,
+} from 'relay-runtime';
+
+import List from './List';
+
+const mountNode = document.getElementById('root');
+
+function fetchQuery(
+  operation,
+  variables,
+) {
+  return fetch('https://api.github.com/graphql', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization: bearer': process.env.TOKEN
+    },
+    body: JSON.stringify({
+      query: operation.text,
+      variables,
+    }),
+  }).then(response => {
+    return response.json();
+  });
+}
+
+const modernEnvironment = new Environment({
+  network: Network.create(fetchQuery),
+  store: new Store(new RecordSource()),
+});
+
+ReactDOM.render(
+  <QueryRenderer
+    environment={modernEnvironment}
+    query={graphql`
+      query AppQuery {
+        gists
+      }
+    `}
+    variables={{}}
+    render={({error, props}) => {
+      if (props) {
+        return <List viewer={props.viewer} />;
+      } else {
+        return <div>Loading</div>;
+      }
+    }}
+  />,
+  mountNode
+);
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require("path");
+const webpack = require("webpack");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const AssetsWebpackPlugin = require("assets-webpack-plugin");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const dotenv = require('dotenv')
+dotenv.config({ path: path.join(__dirname, '.env') })
+
+const input = path.resolve(__dirname, "src");
+const output = path.resolve(__dirname, "public");
+
+module.exports = {
+  devServer: {
+    contentBase: './src'
+  },
+  module: {
+    rules: [{
+      test: /\.jsx?$/,
+      use: [{
+        loader: "babel-loader",
+        query: {
+          cacheDirectory: true,
+        },
+      }],
+      exclude: /node_modules/,
+    }],
+  },
+  context: input,
+  entry: {
+    index: ["./App.jsx"],
+  },
+  output: {
+    filename: "[name].js",
+    path: output,
+  },
+  devtool: "source-map",
+  resolve: {
+    extensions: [".js", ".jsx"],
+    modules: [
+      input,
+      "node_modules",
+    ],
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        TOKEN: JSON.stringify(process.env.TOKEN),
+      },
+    }),
+  ]
+};
